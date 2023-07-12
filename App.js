2b1a056f93a315e650b8c8bff1d557b5a06805c5
ladisLAus2 <koliadiuk_vlad@ukr.net>
2023-07-12 17:11:24 +0300

    ready

---
+++ App.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const fs = require("fs");
+
+const app = express();
+const upload = multer({ dest: "uploads/" });
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+});
+
+// Створення моделі Product з використанням визначеної схеми
+const Product = mongoose.model("Product", productSchema);
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:1234512345@productsapi.o5faifz.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(3001, () => {
+      console.log("Server listening on port 3001");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/postdata", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/getalldata", (req, res) => {
+  // Знайти всі записи в колекції "products"
+  Product.find()
+    .then((products) => {
+      res.json(products);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send("Помилка бази даних при отриманні даних");
+    });
+});
+
+app.post("/products", upload.single("file"), (req, res) => {
+  // Очистити попередні дані перед завантаженням нових
+  Product.deleteMany({})
+    .then(() => {
+      // Отримуємо завантажений файл
+      const filePath = req.file.path;
+
+      // Читаємо вміст файлу
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send("Помилка при читанні файлу");
+        }
+
+        // Перетворюємо JSON дані в об'єкт
+        let products;
+        try {
+          products = JSON.parse(data);
+        } catch (parseError) {
+          console.log(parseError);
+          return res.status(400).send("Некоректний формат JSON");
+        }
+
+        // Зберігаємо дані до MongoDB
+        Product.insertMany(products)
+          .then(() => {
+            // Видалити завантажений файл після обробки
+            fs.unlink(filePath, (unlinkErr) => {
+              if (unlinkErr) {
+                console.log(unlinkErr);
+              }
+            });
+
+            res.status(200).send("Дані успішно завантажено до бази даних");
+          })
+          .catch((dbError) => {
+            console.log(dbError);
+            res.status(500).send("Помилка бази даних під час збереження даних");
+          });
+      });
+    })
+    .catch((dbError) => {
+      console.log(dbError);
+      res
+        .status(500)
+        .send("Помилка бази даних під час очищення попередніх даних");
+    });
+});
---
+++ products.js
@@ -0,0 +1,116 @@
+const products = [
+  {
+    id: 2,
+    name: "Ковбаса салямі312",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі12313",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі123",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі12",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі11",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі9",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі8",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі7",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі6",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі5",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі4",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі3",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса 1",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+  {
+    id: 2,
+    name: "Ковбаса салямі2",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKOhNkpy41LCT8gW9Etf2wmcyVIVynLLZC9w&usqp=CAU",
+    price: 150,
+    description: "Ароматна салямі ковбаса зі спеціями та ніжним смаком.",
+  },
+];
+
+module.exports = products;
